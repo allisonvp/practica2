b8d31bf2f88e2364580d045fe87fd57a68b13847
katty <a20160555@pucp.edu.pe>
2020-04-26 16:03:42 -0500

    html region katty

---
+++ src/main/java/com/example/practica2/Controller/RegionController.java
@@ -0,0 +1,62 @@
+package com.example.practica2.Controller;
+
+import com.example.practica2.Entity.Region;
+import com.example.practica2.Repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value = "/regions")
+public class RegionController {
+
+    @Autowired
+    RegionRepository regionRepository;
+
+    @GetMapping("/list")
+    public String listarRegiones(Model model) {
+        List<Region> listaRegion = regionRepository.findAll();
+        model.addAttribute("lista", listaRegion);
+        return "region/listar";
+    }
+
+    @GetMapping("/create")
+    public String crearRegion() {
+        return "region/crear";
+    }
+
+    @PostMapping("/save")
+    public String guardarRegion(Region region) {
+        regionRepository.save(region);
+        return "redirect:/regions/list";
+    }
+
+    @GetMapping("/edit")
+    public String editarRegion(@RequestParam("id") int id,
+                               Model model){
+        Optional<Region> opt = regionRepository.findById(id);
+        if (opt.isPresent()) {
+            Region region =opt.get();
+            model.addAttribute("region", region);
+            return "/region/editar";
+        } else {
+            return "redirect:/regions/list";
+        }
+    }
+
+    @GetMapping("/delete")
+    public String eliminarRegion(@RequestParam("id") int id) {
+        Optional<Region> opt = regionRepository.findById(id);
+        if (opt.isPresent()) {
+            regionRepository.deleteById(id);
+        }
+        return "redirect:/regions/list";
+    }
+}
