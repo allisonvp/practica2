3d8f5f48eba1ae0f40e2ea02f3a1af24115341da
Alonso Guevara <a20160679@pucp.edu.pe>
2020-04-26 16:15:37 -0500

    employeeEntity

---
+++ src/main/java/com/example/practica2/Entity/Employee.java
@@ -0,0 +1,112 @@
+package com.example.practica2.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "employees")
+public class Employee {
+    @Id
+    private String employee_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String phone_number;
+    private LocalDateTime hire_date;
+    private String job_id;
+    private BigDecimal salary;
+    private BigDecimal commission_pct;
+    private String manager_id;
+    private int department_id;
+
+    public String getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public LocalDateTime getHire_date() {
+        return hire_date;
+    }
+
+    public void setHire_date(LocalDateTime hire_date) {
+        this.hire_date = hire_date;
+    }
+
+    public String getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(String job_id) {
+        this.job_id = job_id;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public BigDecimal getCommission_pct() {
+        return commission_pct;
+    }
+
+    public void setCommission_pct(BigDecimal commission_pct) {
+        this.commission_pct = commission_pct;
+    }
+
+    public String getManager_id() {
+        return manager_id;
+    }
+
+    public void setManager_id(String manager_id) {
+        this.manager_id = manager_id;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+}
